cf6becf6f0e63eda70141c2e98c495698631518e
MariaMsschs <maria.msschs@gmail.com>
2024-08-20 10:55:04 -0300

    Tela de foto

---
+++ app/src/main/java/com/example/mobile_athleta/TelaFoto.java
@@ -0,0 +1,14 @@
+package com.example.mobile_athleta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaFoto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_foto);
+    }
+}

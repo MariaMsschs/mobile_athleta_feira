9722ad517fa4377ed3ce5b0d13e045d6678f79ab
MariaMsschs <maria.msschs@gmail.com>
2024-10-06 21:09:17 -0300

    alteracoes finalizadas

--- app/src/main/java/com/example/mobile_athleta/TelaFoto.java
+++ app/src/main/java/com/example/mobile_athleta/TelaFoto.java
@@ -1,21 +1,24 @@
 package com.example.mobile_athleta;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.widget.ImageView;
-
+import android.view.LayoutInflater;
+import android.view.View;
 import com.bumptech.glide.Glide;
 import com.example.mobile_athleta.databinding.ActivityTelaFotoBinding;
 
 public class TelaFoto extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
-
     private ActivityTelaFotoBinding binding;
 
     @Override
@@ -25,38 +28,76 @@ public class TelaFoto extends AppCompatActivity {
         binding = ActivityTelaFotoBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.camera.setOnClickListener(view -> {
-            dispatchTakePictureIntent();
-        });
-
         binding.botaoVoltar.setOnClickListener(view -> {
             Intent intent = new Intent(TelaFoto.this, TelaCadastro.class);
             startActivity(intent);
             finish();
         });
-    }
 
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
+        binding.camera.setOnClickListener(view -> {
+            // Infle o layout personalizado do diálogo
+            LayoutInflater inflater = getLayoutInflater();
+            View dialogView = inflater.inflate(R.layout.alert_dialog, null);
+
+            // Crie o diálogo com o layout personalizado
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setView(dialogView);
+
+            AlertDialog dialog = builder.create();
+            dialog.show();
+
+
+            // Obtenha referências para os botões no layout do diálogo
+            View tirarFotoButton = dialogView.findViewById(R.id.tirar_foto);
+            View abrirGaleriaButton = dialogView.findViewById(R.id.abrir_galeria);
+
+            // Defina as ações para os botões
+            tirarFotoButton.setOnClickListener(v -> {
+                Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (camera.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(camera, REQUEST_IMAGE_CAPTURE);
+                }
+                dialog.dismiss();
+            });
+
+            abrirGaleriaButton.setOnClickListener(v -> {
+                Intent galeria = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                resultLauncherGaleria.launch(galeria);
+                dialog.dismiss();
+            });
+        });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            if (data != null && data.getExtras() != null) {
+                Bitmap imageBitmap = (Bitmap) data.getExtras().get("data");
 
-            Glide.with(this)
-                    .load(imageBitmap)
-                    .circleCrop()
-                    .into(binding.camera);
+                Glide.with(this)
+                        .load(imageBitmap)
+                        .circleCrop()
+                        .into(binding.camera);
 
-            binding.botaoCadastro.setBackgroundResource(R.drawable.button_design);
-            binding.botaoCadastro.setTextColor(getResources().getColor(R.color.white));
+                binding.botaoCadastro.setBackgroundResource(R.drawable.button_design);
+                binding.botaoCadastro.setTextColor(getResources().getColor(R.color.white));
+            }
         }
     }
+
+    private final ActivityResultLauncher<Intent> resultLauncherGaleria = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(), result -> {
+                if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                    Uri imageUri = result.getData().getData();
+                    Glide.with(this)
+                            .load(imageUri)
+                            .circleCrop()
+                            .into(binding.camera);
+
+                    binding.botaoCadastro.setBackgroundResource(R.drawable.button_design);
+                    binding.botaoCadastro.setTextColor(getResources().getColor(R.color.white));
+                }
+            }
+    );
 }

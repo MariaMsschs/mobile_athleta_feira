fb7783906f48654e39eb34c71555d9ffbd300171
MariaMsschs <maria.msschs@gmail.com>
2024-10-07 08:27:48 -0300

    scroll alterado

--- app/src/main/java/com/example/mobile_athleta/TelaFoto.java
+++ app/src/main/java/com/example/mobile_athleta/TelaFoto.java
@@ -1,5 +1,4 @@
 package com.example.mobile_athleta;
-
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
@@ -16,36 +15,27 @@ import com.bumptech.glide.Glide;
 import com.example.mobile_athleta.databinding.ActivityTelaFotoBinding;
 
 public class TelaFoto extends AppCompatActivity {
-
     static final int REQUEST_IMAGE_CAPTURE = 1;
     private ActivityTelaFotoBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityTelaFotoBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         binding.botaoVoltar.setOnClickListener(view -> {
             Intent intent = new Intent(TelaFoto.this, TelaCadastro.class);
             startActivity(intent);
             finish();
         });
-
         binding.camera.setOnClickListener(view -> {
             LayoutInflater inflater = getLayoutInflater();
             View dialogView = inflater.inflate(R.layout.alert_dialog, null);
-
             View tirarFoto = dialogView.findViewById(R.id.tirar_foto);
             View abrirGaleria = dialogView.findViewById(R.id.abrir_galeria);
-
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setView(dialogView);
-
             AlertDialog dialog = builder.create();
             dialog.show();
-
             tirarFoto.setOnClickListener(v -> {
                 Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 if (camera.resolveActivity(getPackageManager()) != null) {
@@ -53,7 +43,6 @@ public class TelaFoto extends AppCompatActivity {
                 }
                 dialog.dismiss();
             });
-
             abrirGaleria.setOnClickListener(v -> {
                 Intent galeria = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 resultLauncherGaleria.launch(galeria);
@@ -61,25 +50,21 @@ public class TelaFoto extends AppCompatActivity {
             });
         });
     }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             if (data != null && data.getExtras() != null) {
                 Bitmap imageBitmap = (Bitmap) data.getExtras().get("data");
-
                 Glide.with(this)
                         .load(imageBitmap)
                         .circleCrop()
                         .into(binding.camera);
-
                 binding.botaoCadastro.setBackgroundResource(R.drawable.button_design);
                 binding.botaoCadastro.setTextColor(getResources().getColor(R.color.white));
             }
         }
     }
-
     private final ActivityResultLauncher<Intent> resultLauncherGaleria = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(), result -> {
                 if (result.getResultCode() == RESULT_OK && result.getData() != null) {
@@ -88,7 +73,6 @@ public class TelaFoto extends AppCompatActivity {
                             .load(imageUri)
                             .circleCrop()
                             .into(binding.camera);
-
                     binding.botaoCadastro.setBackgroundResource(R.drawable.button_design);
                     binding.botaoCadastro.setTextColor(getResources().getColor(R.color.white));
                 }

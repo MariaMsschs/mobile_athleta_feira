dc51777821f54c9184948c6ce48fa2741800051b
Heitorkatsuki <heitor.katsuki@germinare.org.br>
2024-10-23 17:03:09 -0300

    adicionando a integração com o whatsapp e ajustes na tela de anuncio

--- app/src/main/java/com/example/mobile_athleta/adapter/AnuncioAdapter.java
+++ app/src/main/java/com/example/mobile_athleta/adapter/AnuncioAdapter.java
@@ -4,15 +4,14 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.bumptech.glide.Glide;
 import com.example.mobile_athleta.R;
-import com.example.mobile_athleta.models.Forum;
 import com.example.mobile_athleta.models.Produto;
 import com.squareup.picasso.Picasso;
 
@@ -21,9 +20,16 @@ import java.util.List;
 public class AnuncioAdapter extends RecyclerView.Adapter<AnuncioAdapter.AnuncioViewHolder> {
 
     private List<Produto> produtoList;
+    private OnItemClickListener onItemClickListener;
 
-    public AnuncioAdapter(List<Produto> produtoList) {
+    public interface OnItemClickListener {
+        void onItemClick(Produto produto);
+    }
+
+
+    public AnuncioAdapter(List<Produto> produtoList, OnItemClickListener onItemClickListener) {
         this.produtoList = produtoList;
+        this.onItemClickListener = onItemClickListener;
     }
 
     @NonNull
@@ -36,7 +42,7 @@ public class AnuncioAdapter extends RecyclerView.Adapter<AnuncioAdapter.AnuncioV
     @Override
     public void onBindViewHolder(@NonNull AnuncioAdapter.AnuncioViewHolder holder, int position) {
         Produto produto = produtoList.get(position);
-        holder.bind(produto);
+        holder.bind(produto, onItemClickListener);
     }
 
     @Override
@@ -48,26 +54,26 @@ public class AnuncioAdapter extends RecyclerView.Adapter<AnuncioAdapter.AnuncioV
         private ImageView imagem_produto;
         private TextView titulo_anuncio, anunciante, preco;
 
-        private Context context;
-
         public AnuncioViewHolder(@NonNull View itemView) {
             super(itemView);
             imagem_produto = itemView.findViewById(R.id.imagem_produto_anuncio);
             titulo_anuncio = itemView.findViewById(R.id.titulo_anuncio);
             anunciante = itemView.findViewById(R.id.autor_anuncio);
             preco = itemView.findViewById(R.id.preco);
-            context = itemView.getContext();
         }
 
-        public void bind(Produto produto) {
-            String imageUrl = produto.getImage();
-//            Glide.with(itemView.getContext()).load(imageUrl).into(imagem_produto);
-            Picasso.get()
-                    .load(imageUrl)
-                    .into(imagem_produto);
+        public void bind(final Produto produto, final OnItemClickListener listener) {
+            Picasso.get().load(produto.getImage()).into(imagem_produto);
             titulo_anuncio.setText(produto.getTitle());
             anunciante.setText(produto.getDescription());
             preco.setText(String.valueOf(produto.getPrice()));
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    listener.onItemClick(produto);
+                }
+            });
         }
     }
 }
